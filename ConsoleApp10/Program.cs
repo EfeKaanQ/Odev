86651af027e9e636652870f5c10ad5669fa7ebf2
efekaan <efekaan@EKD>
2023-12-11 23:01:03 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Öğrenci ve ders listelerini oluştur
+           
+            List<string> ogrenciler = new List<string>{ "kadir","Efe","Mehmet","sadettin","samet","ahmet turan","Necati","yaşar","selim","muzaffer","eyüp ali","özlem","hacı ramazan","ayşenur","erdem","mahmut","bedirhan","hasan","levent","ali berat","abdulmuttalip","muhammed eren","selehaddin","yeliz","samed","burak arif","yağız","ahmet" };
+            for (int i = 1; i <= 0; i++)
+            {
+                ogrenciler.Add(ogrenciler[i]) ;
+            }
+
+            List<string> dersler = new List<string> { "Matematik", "Fizik", "Kimya", "Biyoloji", "Nesne Tabanlı", "robotik kodlama", "Beden", "edebiyat", "tarih", "felsefe", "ingilizce", "din kültürü" };
+            for (int i = 1; i <= 0; i++)
+            {
+                dersler.Add(dersler[i]);
+            }
+
+           //int i =1;<=30;i++ bu soruyu hocana sor
+            Random random = new Random();
+
+            foreach (var ogrenci in ogrenciler)
+            {
+                
+                string randomDers = dersler[random.Next(dersler.Count)];
+
+                
+                Console.WriteLine($"{ogrenci} öğrencisine :  {randomDers} Dersi VERİLMİŞTİRRRRRR ♥");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+    
+
